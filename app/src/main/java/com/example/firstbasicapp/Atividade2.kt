e7473135bee4be8731c067d29ed629cd79e7cf80
Vinicius Petratti <viniciuspetratti@hotmail.com>
2023-02-13 19:51:05 -0300

    First commit

---
+++ app/src/main/java/com/example/firstbasicapp/Atividade2.kt
@@ -0,0 +1,11 @@
+package com.example.firstbasicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Atividade2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atividade2)
+    }
+}
